a2bf48a69aed32d97c0e1bbba7b3bd652b771580
YawEffah <effahofficial99@gmail.com>
2023-12-01 14:06:37 +0000

    scroll-top icon modified

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
