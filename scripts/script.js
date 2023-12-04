93678daf90e144d189bbdfec470c96d59e53e7cc
YawEffah <effahofficial99@gmail.com>
2023-12-04 17:38:40 +0000

    Landing page modified

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 200px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
